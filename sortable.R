726a104112b853c3cbe80671b55d9a163f04e14d
Saurfang <forest.fang@outlook.com>
2014-09-20 21:42:06 -0400

    make checkbox sortable fire event on change

--- sortable.R
+++ sortable.R
@@ -8,18 +8,13 @@ sortableCheckboxGroupInput <- function(inputId, ...) {
   # return label and select tag
   attachDependencies(
     tagList(
+      singleton(tags$head(tags$script(src="js/sortable.js"))),
       checkboxGroupInput(inputId, ...),
-      tags$script(paste0("$('#", inputId, "').sortable({
-        //propogate changes in position directly to the element being moved
-        //TODO: How can we trigger the event correctly while the drag is in progress? (i.e. provide realtime update when desired)
-        update: function( event, ui ) {
-          $(event.target).trigger('change');
-        }
-      });"))),
+      tags$script(paste0("makeSortable($('#", inputId, "'));"))),
     jqueryUIDep)
 }
 
-sortableSelectizeInput <- function (inputId, ..., options = NULL, width = NULL) 
+sortableSelectizeInput <- function (..., options = NULL) 
 {
   # build jquery-ui dependency for sortable
   jqueryUIDep <- htmlDependency("jqueryui", "1.10.4", c(href="shared/jqueryui/1.10.4"),
@@ -31,14 +26,5 @@ sortableSelectizeInput <- function (inputId, ..., options = NULL, width = NULL)
   options$plugins <- c(list("drag_drop"), options$plugins)
   
   # return label and select tag
-  attachDependencies(
-    tagList(
-      shiny:::selectizeIt(inputId, selectInput(inputId, ..., selectize = FALSE), options, width),
-      tags$script(paste0("$('#", inputId, "').on(
-        //propogate changes in position directly to the element being moved
-        //TODO: How can we trigger the event correctly while the drag is in progress? (i.e. provide realtime update when desired)
-        'sortupdate', function( event, ui ) {
-          $(event.target).trigger('change');
-        });"))),
-    jqueryUIDep)
+  attachDependencies(selectizeInput(..., options = options), list(jqueryUIDep))
 }
---
+++ www/js/sortable.js
@@ -0,0 +1,34 @@
+function makeSortable(el) {
+    el.sortable({
+        //clone a helper which will append to the end so that it would visually
+        //preserve the checked status while dragging
+        helper: "clone",
+        placeholder: {
+            element: function(item) {
+              //clone the item and show it so the placeholder replicate the item 
+              //and will be reflected in our values
+              var clone = item.clone();
+              clone.show();
+              
+              //uncheck the box so the original box won't mess with our values
+              var checkbox = item.children(":checkbox");
+              checkbox.prop('was-checked', checkbox.prop('checked'));
+              checkbox.prop('checked', false);
+          
+              return clone;
+            },
+            update: function() {
+                return;
+            }
+        },
+        change: function(event, ui) {
+            //propogate changes in position directly to the element being moved
+            $(event.target).trigger('change');
+        },
+        stop: function(event, ui) {
+          //now we want to restore the checked status if it was checked
+          var checkbox = ui.item.children(":checkbox");
+          checkbox.prop('checked', checkbox.prop('was-checked'));
+        }
+    });
+}
